5eb455784a0f7edb431813f8cf1feda30568fe63
Gabriel Freire <gabrielfvale@gmail.com>
2019-01-26 17:59:52 -0300

    Refactor StatelessWidget to StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,32 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(App());
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  createState() => AppState();
+}
+
+class AppState extends State<App> {
+
+  int counter = 0;
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Flutter Training App'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => setState(() => counter++),
+          child: Icon(Icons.add),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('You hit the FAB $counter times.')
+            ],
+          ),
         ),
       ),
     );
